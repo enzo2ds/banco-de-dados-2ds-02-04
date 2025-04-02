8025f24f0c4ea4881283c82784dc6742eff633f4
enzo2ds <enzo.kauan.carvalho@escola.pr.gov.br>
2025-04-02 10:38:18 -0300

    Create banco-festas.sql

---
+++ banco-festas.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Cliente 
+( 
+); 
+
+CREATE TABLE Festas 
+( 
+); 
+
+CREATE TABLE Pagamento 
+( 
+); 
+
+CREATE TABLE Pix 
+( 
+ Codigo_pix INT,  
+); 
+
+CREATE TABLE CartaoCredito 
+( 
+ Nome no cartao INT,  
+ Endereço INT,  
+ CPF INT,  
+); 
+
+CREATE TABLE Boleto 
+( 
+ Data_vencimento INT,  
+ Cod_barras INT,  
+); 
